5dea541c92261f1e580071587f5eceac75d00e1b
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-07-30 23:12:35 -0400

    final

--- App.js
+++ App.js
@@ -1,205 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { Font } from 'expo';
 
-const qna = {
-  'questions':[
-    {
-      'title': 'El Lavado Perfecto lo haces con:',
-      'answers': {
-        'a': 'Detergentes – Lavalozas – Quitamanchas',
-        'b': 'Detergentes – Suavizantes – Quitamanchas',
-        'c': 'Detergentes – Limpiadores – Quitamanchas'
-      },
-      'win': {
-        1: false,
-        2: true,
-        3: false
-      }
-    },
-    {
-      'title': 'Los Detergentes de Ferias no son recomendados por:',
-      'answers': {
-        'a': 'Tienen menor dosificación por lavado',
-        'b': 'Tienen un bajo nivel de activos en su formulación',
-        'c': 'Tienen muchos aditivos'
-      },
-      'win': {
-        1: false,
-        2: true,
-        3: false
-      }
-    },
-    {
-      'title': '¿Por qué el cambio de Bolsas versus Cajas?',
-      'answers': {
-        'a': 'Las Cajas no son reciclables',
-        'b': 'Las Cajas tiene menor durabilidad',
-        'c': 'Las Cajas cuesta más el armado'
-      },
-      'win': {
-        1: true,
-        2: false,
-        3: false
-      }
-    },
-    {
-      'title': 'Las nuevas Bolsas de detergentes son:',
-      'answers': {
-        'a': '100% Reutilizables',
-        'b': '100% Reciclables',
-        'c': '100% Sustentables'
-      },
-      'win': {
-        1: false,
-        2: true,
-        3: false
-      }
-    },
-    {
-      'title': 'Todas las Bolsas de Detergentes tienen un logo que dice:',
-      'answers': {
-        'a': 'Eco Saco 100% Biodegradable',
-        'b': 'Eco Bolsa 100% Reutilizables',
-        'c': 'Eco Bolsa 100% Reciclables'
-      },
-      'win': {
-        1: false,
-        2: false,
-        3: true
-      }
-    },
-    {
-      'title': 'El suavizante Soft cuantos beneficios tiene:',
-      'answers': {
-        'a': '4 beneficios',
-        'b': '5 beneficios',
-        'c': '6 beneficios'
-      },
-      'win': {
-        1: false,
-        2: true,
-        3: false
-      }
-    },
-    {
-      'title': 'OMO y Soft Piel sensible ayudan:',
-      'answers': {
-        'a': 'Deja tu ropa limpia y suave con un olor exquisito',
-        'b': 'Dejan tu ropa suave y fácil de planchar',
-        'c': 'Deja impecable y con olor increíble'
-      },
-      'win': {
-        1: true,
-        2: false,
-        3: false
-      }
-    },
-    {
-      'title': 'Los detergentes de nuestras marcas están:',
-      'answers': {
-        'a': 'Tienen mayores niveles de activos de Limpieza',
-        'b': 'Tienen bajo performance',
-        'c': 'Tienen altos preservantes en la formula'
-      },
-      'win': {
-        1: true,
-        2: false,
-        3: false
-      }
-    },
-    {
-      'title': 'La publicidad de OMO Quitamanchas dice:',
-      'answers': {
-        'a': 'Saca todo tipo de Machas',
-        'b': 'Remueve las manchas difíciles',
-        'c': 'Elimina las manchas de chocolate'
-      },
-      'win': {
-        1: false,
-        2: true,
-        3: false
-      }
-    },
-    {
-      'title': 'Soft que hace en tu ropa:',
-      'answers': {
-        'a': 'Mantiene Limpia y Fresca tu ropa al contacto con tu piel',
-        'b': 'Mantiene las fibras lisas, para que la ropa se sienta suave y delicada al tacto, con un toque suave de fragancia',
-        'c': 'Mantiene las fibras alineadas y ordenas con un rico y suave perfume'
-      },
-      'win': {
-        1: false,
-        2: true,
-        3: false
-      }
-    },
-    {
-      'title': 'Soft y OMO Piel Sensible es solo para:',
-      'answers': {
-        'a': 'Solo ropa de guagua',
-        'b': 'Ropa de guagua, ropa interior y todo tipo de prendas',
-        'c': 'Solo ropa de adultos'
-      },
-      'win': {
-        1: false,
-        2: true,
-        3: false
-      }
-    },
-    {
-      'title': 'Las nuevas Bolsas se Detergentes que beneficios poseen:',
-      'answers': {
-        'a': 'Son de PE, material 100% reciclable',
-        'b': 'Son de Plástico, 99% reutilizable',
-        'c': 'Son Ecológicas 100% Biodegradables'
-      },
-      'win': {
-        1: true,
-        2: false,
-        3: false
-      }
-    },
-    {
-      'title': 'La tarea de un detergente es:',
-      'answers': {
-        'a': 'Eliminar la suciedad y sacar manchas',
-        'b': 'No dañar y Perfumar la ropa',
-        'c': 'Eliminar olores y Suciedad'
-      },
-      'win': {
-        1: false,
-        2: true,
-        3: false
-      }
-    },
-    {
-      'title': 'La dosificación del detergente se calcula según:',
-      'answers': {
-        'a': 'El grado de suciedad de la ropa y la dureza del agua',
-        'b': 'Las machas y tipo de ropa a lavar',
-        'c': 'El tipo de lavadora y tipo de ropa'
-      },
-      'win': {
-        1: true,
-        2: false,
-        3: false
-      }
-    },
-    {
-      'title': '¿Por qué es necesario un suavizante?',
-      'answers': {
-        'a': 'El suavizante perfuma y suaviza la ropa',
-        'b': 'Retorna la flexibilidad a la ropa, recomponiendo las fibras y además perfumándolas',
-        'c': 'Hace la ropa dure más, además las deja con una agradable fragancia'
-      },
-      'win': {
-        1: false,
-        2: true,
-        3: false
-      }
-    }
-  ]
-}
+import qna from './assets/trivia.json';
 let current = 0;
 let score = 0;
 let trivia = {};
@@ -212,6 +15,11 @@ export default class App extends React.Component {
       active: false,
       next: false,
       current: current,
+      optionA: false,
+      optionB: false,
+      optionC: false,
+      end: false,
+      endText: '',
       title: '',
       answers: {
         a:'',
@@ -220,30 +28,69 @@ export default class App extends React.Component {
       }
     };
   }
+  componentDidMount() {
+    Font.loadAsync({
+      'Montserrat-Regular': require('./fonts/Montserrat-Regular.ttf'),
+    });
+  }
   shuffleTrivia() {
     trivia = __.shuffle(qna.questions);
     this.next();
   }
+  begin() {
+    current = 0;
+    this.setState({
+      active: false,
+      next: false,
+      current: current,
+      optionA: false,
+      optionB: false,
+      optionC: false,
+      end: false,
+      endText: '',
+      title: '',
+      answers: {
+        a:'',
+        b:'',
+        c:''
+      }
+    });
+  }
   check(option) {
-    if( trivia[current].win[option] == true ){
+    if( trivia[current].win[option] == true && this.state.next == false ){
       score++;
     }
+    this.setState({optionA:false, optionB:false, optionC:false});
+    __.each(trivia[current].win, (val, key)=>{
+      if(parseInt(key) === 1 && val === true){ this.setState({optionA:true}); }
+      if(parseInt(key) === 2 && val === true){ this.setState({optionB:true}); }
+      if(parseInt(key) === 3 && val === true){ this.setState({optionC:true}); }
+    });
     this.setState({
       score: score,
       next: true
     });
   }
   next() {
-    current++;
-    if( current > 3){
+    if( current == 3){
+      if(score >= 3){
+        this.setState({
+          endText: '¡Felicitaciones has ganado el desafío del Lavado Perfecto!'
+        });
+      }else{
+        this.setState({
+          endText: '¡Sigue participando!'
+        });
+      }
       current = 0;
       score = 0;
       this.setState({
-        next: false,
-        active: false
+        end: true
       });
     }else{
+      current++;
       this.setState({
+        end: false,
         active: true,
         current: current,
         score: score
@@ -264,43 +111,157 @@ export default class App extends React.Component {
         }}>
           <Image
           source={require('./assets/bg.png')}/>
-          <TouchableOpacity onPress={this.shuffleTrivia.bind(this)} style={{position:'absolute',top:'50%'}}>
-            <Text>Empezar</Text>
-          </TouchableOpacity>
+          <View style={
+            {
+              position: 'absolute',
+              top: 0,
+              width: '100%',
+              height: '100%',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}>
+            <Image
+              style={{position:'absolute', top:45}}
+              source={require('./assets/logo.png')}/>
+            <TouchableOpacity onPress={this.shuffleTrivia.bind(this)} style={{position:'absolute', top:375}}>
+              <Image
+                source={require('./assets/begin.png')}/>
+            </TouchableOpacity>
+            <Image
+              style={{position:'absolute', bottom:0}}
+              source={require('./assets/washers.png')}/>
+          </View>
+          
         </View>
         <View style={{
           opacity: this.state.active ? 1 : 0,
           height: this.state.active ? 'auto' : 0
         }}>
           <Image
-          source={require('./assets/bg-blur.png')}/>
-          <View style={{position:'absolute'}}>
-            <Text>{this.state.current}</Text>
-            <Text>Puntos: {this.state.score}</Text>
-            <Text>{this.state.title}</Text>
-            <Text>A) {this.state.answers.a}</Text>
-            <Text>B) {this.state.answers.b}</Text>
-            <Text>C) {this.state.answers.c}</Text>
-            <View style={{
-              opacity: this.state.next ? 0 : 1,
-              height: this.state.next ? 0 : 'auto'
+          source={require('./assets/bg.png')}/>
+          <View style={
+            {
+              position: 'absolute',
+              top: 0,
+              width: '100%',
+              height: '100%',
+              alignItems: 'center',
+              justifyContent: 'center'
             }}>
-              <TouchableOpacity onPress={()=>this.check(1)}>
-                <Text>A</Text>
+            <Image
+              style={{position:'absolute', top:45}}
+              source={require('./assets/logo.png')}/>
+            <Image
+              style={{position:'absolute', top:375}}
+              source={require('./assets/bg-q.png')}/>
+            <View style={{
+              width: '80%',
+              opacity: this.state.end ? 1 : 0,
+              height: this.state.end ? 'auto' : 0
+            }}>
+              <Text style={
+                {
+                  // fontFamily: 'Montserrat-Regular',
+                  fontSize: 36,
+                  marginBottom: 8,
+                  color: '#3D3D3D',
+                  backgroundColor: 'transparent'}
+                }>
+                {this.state.endText}
+              </Text>
+            </View>
+            <View style={{
+              width: '80%',
+              opacity: this.state.end ? 0 : 1,
+              height: this.state.end ? 0 : 'auto'
+            }}>
+              <Text style={
+                {
+                  // fontFamily: 'Montserrat-Regular',
+                  fontSize: 36,
+                  marginBottom: 8,
+                  color: '#3D3D3D',
+                  backgroundColor: 'transparent'}
+                }>
+                {this.state.title}
+              </Text>
+              <Text style={
+                {
+                  // fontFamily: 'Montserrat-Regular',
+                  fontSize: 26,
+                  color: '#3D3D3D',
+                  backgroundColor: 'transparent'}
+                }>A) {this.state.answers.a}</Text>
+              <Text style={
+                {
+                  fontSize: 26,
+                  color: '#3D3D3D',
+                  backgroundColor: 'transparent'}
+                }>B) {this.state.answers.b}</Text>
+              <Text style={
+                {
+                  // fontFamily: 'Montserrat-Regular',
+                  fontSize: 26,
+                  color: '#3D3D3D',
+                  backgroundColor: 'transparent'}
+                }>C) {this.state.answers.c}</Text>
+            </View>
+            <Text style={
+              {
+                position: 'absolute',
+                bottom: 350,
+                fontSize: 26,
+                color: '#3D3D3D',
+                opacity: this.state.end ? 0 : 1,
+                height: this.state.end ? 0 : 'auto',
+                backgroundColor: 'transparent'}
+              }>{this.state.current} de 3</Text>
+            <View style={{
+              position: 'absolute',
+              bottom: 90,
+              width: '100%',
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              opacity: this.state.active && !this.state.end ? 1 : 0,
+              height: this.state.active && !this.state.end ? 'auto' : 0
+            }}>
+              <TouchableOpacity style={styles.options} onPress={()=>this.check(1)}>
+                {this.state.next && this.state.optionA===true ? <Image source={require('./assets/good.png')} style={styles.result}/> : null}
+                {this.state.next && this.state.optionA===false ? <Image source={require('./assets/bad.png')} style={styles.result}/> : null}
+                <Image source={require('./assets/a.png')}/>
               </TouchableOpacity>
-              <TouchableOpacity onPress={()=>this.check(2)}>
-                <Text>B</Text>
+              <TouchableOpacity style={styles.options} onPress={()=>this.check(2)}>
+                {this.state.next && this.state.optionB===true ? <Image source={require('./assets/good.png')} style={styles.result}/> : null}
+                {this.state.next && this.state.optionB===false ? <Image source={require('./assets/bad.png')} style={styles.result}/> : null}
+                <Image source={require('./assets/b.png')}/>
               </TouchableOpacity>
-              <TouchableOpacity onPress={()=>this.check(3)}>
-                <Text>C</Text>
+              <TouchableOpacity style={styles.options} onPress={()=>this.check(3)}>
+                {this.state.next && this.state.optionC===true ? <Image source={require('./assets/good.png')} style={styles.result}/> : null}
+                {this.state.next && this.state.optionC===false ? <Image source={require('./assets/bad.png')} style={styles.result}/> : null}
+                <Image source={require('./assets/c.png')}/>
               </TouchableOpacity>
             </View>
             <View style={{
               opacity: this.state.next ? 1 : 0,
-              height: this.state.next ? 'auto' : 0
+              height: this.state.next ? 'auto' : 0,
+              position: 'absolute',
+              bottom:260
             }}>
               <TouchableOpacity onPress={()=>this.next()}>
-                <Text>Siguiente</Text>
+                <Image source={require('./assets/next.png')}/>
+              </TouchableOpacity>
+            </View>
+            <View style={{
+              opacity: this.state.end ? 1 : 0,
+              height: this.state.end ? 'auto' : 0,
+              position: 'absolute',
+              zIndex:99,
+              bottom:260
+            }}>
+              <TouchableOpacity onPress={()=>this.begin()}>
+                <Image source={require('./assets/startover.png')}/>
               </TouchableOpacity>
             </View>
           </View>
@@ -312,6 +273,17 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  result: {
+    position: 'absolute',
+    zIndex: 99,
+    top: 5,
+    right: -20
+  },
+  options: {
+    height: 100,
+    marginLeft: 30,
+    marginRight: 30
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
